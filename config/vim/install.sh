ffbe98e41adc9dcca002514f81058bbb122f9af2
EL Djabro <mohamed.yacine.djabri@gmail.com>
2018-04-11 11:27:19 +0200

    removed PluginInstall: YouCompleteMe from vimrc and added to install.sh because some modules cannot be imported -- fixed with cmd \'git submodule update --init --recursive\'

--- config/vim/install.sh
+++ config/vim/install.sh
@@ -14,6 +14,9 @@ mkdir -p ~/.vim/bundle && \
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall
 
-# Finalize YouCompleteMe install
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
+#  YouCompleteMe install
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
